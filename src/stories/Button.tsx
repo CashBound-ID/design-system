5d5740a4f1ed6c43c8d59daa46ad29f00f89572b
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2024-11-17 18:09:53 +0700

    feat(docs): adjust button stories for enable show how to used section on stories

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,17 +1,12 @@
 import { genCSSVariableStyle } from '@/utils/theme';
 
-import './button.css';
+import './button.scss';
 
 export interface ButtonProps {
-  /** What background color to use */
   backgroundColor?: string;
-  /** Button contents */
   label: string;
-  /** Optional click handler */
   onClick?: () => void;
-  /** Is this the principal call to action on the page? */
   primary?: boolean;
-  /** How large should the button be? */
   size?: 'small' | 'medium' | 'large';
 }
 
