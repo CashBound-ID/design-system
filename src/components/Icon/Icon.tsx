ebc203b57fad01f8259d20675c59304e1ffed5ea
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2024-11-18 22:04:37 +0700

    feat(component): create icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,54 @@
+import type { HTMLAttributes } from 'react';
+
+import { cx } from '@/utils/css';
+import type { CashboundIconType } from '@/types/icon';
+import type { GenericHTMLProps } from '@/types/react';
+
+type SpanHTMLProps = GenericHTMLProps<HTMLAttributes<HTMLSpanElement>>;
+
+interface IconProps extends SpanHTMLProps {
+  /** The color of the icon */
+  color?: string;
+  /** The type of the icon */
+  icon: CashboundIconType;
+  /** The margin around the icon */
+  margin?: number | string;
+  /** The size of the icon */
+  size?: number;
+}
+
+const Icon = (props: IconProps) => {
+  const {
+    'aria-label': ariaLabel = 'fithub icon',
+    className,
+    color,
+    icon,
+    margin,
+    role = 'img',
+    size = 16,
+    ...res
+  } = props;
+
+  return (
+    <span
+      {...res}
+      style={{
+        color,
+        fontSize: size,
+        height: size,
+        margin,
+        position: 'relative',
+        textAlign: 'center',
+        width: size
+      }}
+      role={role}
+      aria-label={ariaLabel}
+      data-icon={icon}
+      className={cx('cashbound-icon', className)}
+    />
+  );
+};
+
+Icon.displayName = 'Icon';
+
+export default Icon;
