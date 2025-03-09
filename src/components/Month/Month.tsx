45e376b06756f21eed42b116cbedf1ac652de757
muhfathurrah16 <muhammadfathur88@gmail.com>
2025-03-09 11:12:55 +0700

    feat(components): create component month

---
+++ src/components/Month/Month.tsx
@@ -0,0 +1,90 @@
+import { useCallback, useState } from 'react';
+
+import Flex from '@/components/Flex';
+import useMount from '@/hooks/useMount';
+
+import { setToFirstDayOfMonth } from '@/utils/date';
+
+import MonthPickerActionBar from './MonthActionBar';
+import MonthPickeritem from './Monthitem';
+import * as styles from './style.module.scss';
+import type { MonthProps, MonthsType } from './types';
+
+const MonthPicker = (props: MonthProps) => {
+  const { onChooseMonth, selectedDate = 0 } = props;
+
+  const [months, setMonths] = useState<MonthsType[]>();
+  const [currentYear, setCurrentYear] = useState<number>(() => 0);
+
+  const getMonthList = () => {
+    const monthList = [];
+    for (let i = 0; i < 12; i++) {
+      const date = new Date(0, i);
+      const monthName = date.toLocaleString('default', { month: 'short' });
+      monthList.push({ name: monthName.toUpperCase(), value: i + 1 });
+    }
+
+    setMonths(monthList);
+    return monthList;
+  };
+
+  const handleOnChangeActionBar = useCallback(
+    (arg: number) => {
+      setCurrentYear(arg);
+    },
+    [setCurrentYear]
+  );
+
+  const handleOnClickMonthItem = useCallback(
+    (month: number) => {
+      const formattedChoosenDate = setToFirstDayOfMonth(
+        new Date(currentYear, month - 1)
+      ).getTime();
+      const currentDate = setToFirstDayOfMonth(
+        new Date(selectedDate)
+      ).getTime();
+
+      if (currentDate && formattedChoosenDate === currentDate) {
+        onChooseMonth(undefined);
+        return;
+      }
+      onChooseMonth(formattedChoosenDate);
+    },
+    [currentYear, onChooseMonth, selectedDate]
+  );
+
+  useMount(() => {
+    let currentDate = new Date();
+
+    if (selectedDate) currentDate = new Date(selectedDate);
+
+    setCurrentYear(currentDate.getFullYear());
+
+    getMonthList();
+  });
+
+  return (
+    <section className={styles['month']} aria-label="month">
+      <MonthPickerActionBar
+        currentYear={currentYear}
+        onChange={handleOnChangeActionBar}
+      />
+      <Flex className={styles['month__content']} wrap="wrap">
+        {months &&
+          months.map((item) => {
+            return (
+              <MonthPickeritem
+                key={item.value}
+                selectedYear={currentYear}
+                months={item}
+                onClickMonthItem={handleOnClickMonthItem}
+                selectedDate={selectedDate}
+              />
+            );
+          })}
+      </Flex>
+    </section>
+  );
+};
+
+export default MonthPicker;
