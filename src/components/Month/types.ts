64bb2bb13824368deea1b62cc3c41e6c4322e9dc
muhfathurrah16 <muhammadfathur88@gmail.com>
2025-03-09 11:12:55 +0700

    feat(components): create component month

---
+++ src/components/Month/types.ts
@@ -0,0 +1,56 @@
+import type { NullAble } from '@/types/utils';
+
+export interface MonthsType {
+  name: string;
+  value: number;
+}
+
+export type MonthProps = {
+  /**
+   * Callback function triggered when a date is selected.
+   * @param {number} [args] - The selected date as a timestamp (in milliseconds).
+   */
+  onChooseMonth: (args?: number) => void;
+
+  /**
+   * The currently selected date as a timestamp (in milliseconds).
+   * Optional.
+   */
+  selectedDate?: number;
+};
+
+export interface MonthActionBarProps {
+  /**
+   * The current year displayed in the action bar.
+   */
+  currentYear: number;
+
+  /**
+   * Callback function triggered when the year is changed.
+   * @param {number} arg - The new year to set.
+   */
+  onChange: (arg: number) => void;
+}
+
+export interface MonthitemProps {
+  /**
+   * Whether the calendar item is disabled.
+   * Optional.
+   */
+  disabled?: boolean;
+  months: MonthsType;
+
+  /**
+   * Callback function triggered when a calendar item is clicked.
+   * @param {number} date - The selected date as a timestamp (in milliseconds).
+   */
+  onClickMonthItem: (string: number) => void;
+
+  /**
+   * The currently selected date as a timestamp (in milliseconds), or null.
+   * Optional.
+   */
+  selectedDate?: NullAble<number>;
+
+  selectedYear: number;
+}
